319722cb39da56df80675f86babe411ae080031e
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-07-23 17:07:55 +0530

    Card created

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import "./index.css";
  * - Search
  * - RestaurantContainer
  *  - RestaurantCard
+ *   - Img
+ *   - Restaurant Name
+ *   - star rating , cuisines, delivery time
  * * Footer
  * - Copyright
  * - links
@@ -37,14 +40,28 @@ const Header = () => {
 };
 
 const RestaurantCard = () => {
-  return <div></div>;
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/FOOD_CATALOG/IMAGES/CMS/2024/4/8/3424de92-9520-481b-8fc6-14ab8855d9e4_0f25daa0-0978-4638-9a94-dcd3d54b4789.JPG"
+        alt="res-logo"
+      />
+      <h3>Bun in a Million</h3>
+      <h4>Burgers, Fast Food, Snacks</h4>
+      <h4>4.4 stars</h4>
+      <h4>25 mins</h4>
+    </div>
+  );
 };
 
 const Body = () => {
   return (
     <div className="body">
-      <div className="Search">Search</div>
-      <div className="res-container"></div>
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+      </div>
     </div>
   );
 };
@@ -53,6 +70,7 @@ const AppLayout = () => {
   return (
     <div className="app">
       <Header />
+      <Body />
     </div>
   );
 };
