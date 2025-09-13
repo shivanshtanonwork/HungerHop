54a9a00d7f88bb5e25f018db578318c9772fad93
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-09-13 19:15:28 +0530

    passing data through props in class component

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,7 +1,7 @@
-const User = () => {
+const User = ({ name }) => {
   return (
     <div className="user-card">
-      <h2>Name: Shivansh</h2>
+      <h2>Name: {name}</h2>
       <h3>Location: Bengaluru</h3>
       <h4>Contact: @shivansh._.tandon</h4>
     </div>
--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
 class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+    // console.log(props);
+  }
   render() {
+    const { name, location } = this.props;
     return (
       <div className="user-card">
-        <h2>Name: Shivansh</h2>
-        <h3>Location: Bengaluru</h3>
+        <h2>Name: {name}</h2>
+        <h3>Location: {location}</h3>
         <h4>Contact: @shivansh._.tandon</h4>
       </div>
     );
