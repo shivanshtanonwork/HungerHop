7eb196115690b1fd935ccc311dfd1aeec4f3866f
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-09-19 19:58:28 +0530

    cart build

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,15 +1,34 @@
 import { useSelector } from "react-redux";
 import ItemList from "./ItemList";
+import { useDispatch } from "react-redux";
+import { clearCart } from "../utils/cartSlice";
 
 const Cart = () => {
   const cartItems = useSelector((store) => store.cart.items);
+
+  const dispatch = useDispatch();
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  };
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100 p-6">
       <div className="w-full max-w-md rounded-2xl bg-white shadow-lg p-8">
-        <h1 className="text-3xl font-bold text-gray-800 text-center mb-4">
+        <h1 className="text-3xl font-bold text-gray-800 text-center mb-6">
           🛒 Cart
         </h1>
-        <div>
+
+        <div className="flex flex-col gap-4">
+          <button
+            className="self-end px-4 py-2 text-sm font-medium text-white bg-red-500 rounded-lg shadow hover:bg-red-600 transition"
+            onClick={handleClearCart}
+          >
+            Clear Cart
+          </button>
+          {cartItems.length === 0 && (
+            <h1 className="font-bold text-center">
+              Cart is Empty. Add items to cart.
+            </h1>
+          )}
           <ItemList items={cartItems} />
         </div>
       </div>
